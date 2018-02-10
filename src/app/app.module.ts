d00ecfb065e7a2d74adbdd21c5971da847a2d751
임우찬 <vhehentm@gmail.com>
2018-02-10 10:46:11 +0900

    TODO one-step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import {RouterModule} from '@angular/router';
+import {routes} from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
