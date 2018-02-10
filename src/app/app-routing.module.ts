3f4c0ad6f8073f6730eb12e5c1296f75c371f840
임우찬 <vhehentm@gmail.com>
2018-02-10 11:07:52 +0900

    TODO two-step

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import {Routes} from '@angular/router';
 import {IndexComponent} from './index/index.component';
+import {JqueryComponent} from './jquery/jquery.component';
+import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
-  {path: '', component: IndexComponent}
+  {path: '', component: IndexComponent, children: [
+    {path: '', component: HomeComponent},
+    {path: 'jquery', component: JqueryComponent}
+  ]}
 ];
